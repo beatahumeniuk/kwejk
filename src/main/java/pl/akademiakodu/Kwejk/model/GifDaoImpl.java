049d5437cb8357d8cfcc2a663ca849c5bc006fce
beata_humeniuk <beatahumeniuk@gmail.com>
2017-11-13 16:12:47 +0100

    all gifs

--- src/main/java/pl/akademiakodu/Kwejk/model/GifDaoImpl.java
+++ src/main/java/pl/akademiakodu/Kwejk/model/GifDaoImpl.java
@@ -9,12 +9,12 @@ public class GifDaoImpl{
    public List<Gif> findAll(){
        List<Gif> gifs = new ArrayList<>();
 
-       gifs.add(new Gif("android-explosion.gif"));
-       gifs.add(new Gif("ben-and-mike.gif"));
-       gifs.add(new Gif("book-dominos.gif"));
-       gifs.add(new Gif("compiler-bot.gif"));
-       gifs.add(new Gif("cowboy-coder.gif"));
-       gifs.add(new Gif("infinite-andrew.gif"));
+       gifs.add(new Gif("android-explosion"));
+       gifs.add(new Gif("ben-and-mike"));
+       gifs.add(new Gif("book-dominos"));
+       gifs.add(new Gif("compiler-bot"));
+       gifs.add(new Gif("cowboy-coder"));
+       gifs.add(new Gif("infinite-andrew"));
        return gifs;
     }
 }
